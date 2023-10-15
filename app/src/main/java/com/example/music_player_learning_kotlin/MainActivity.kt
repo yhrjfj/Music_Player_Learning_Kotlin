69d315cfee2817e638a22cac8a0f2a8fabbacaab
Shinigami <102893295+shadowlight671@users.noreply.github.com>
2023-10-15 14:04:45 +0600

    Done

--- app/src/main/java/com/example/music_player_learning_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/music_player_learning_kotlin/MainActivity.kt
@@ -3,24 +3,128 @@ package com.example.music_player_learning_kotlin
 import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.provider.MediaStore.Audio.Media
 import android.widget.Button
 import android.widget.SeekBar
 import android.widget.TextView
+import android.widget.Toast
+import com.example.music_player_learning_kotlin.R
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
+
+    // variables
+    var startTime = 0.0
+    var finalTime = 0.0
+    var forwardTime = 10000
+    var backwardTime = 10000
+    var oneTimeOnly = 0
+
+    // Handler
+    var handler: Handler = Handler()
+
+    // Media Player
+    var mediaPlayer = MediaPlayer()
+    lateinit var time_txt: TextView
+    lateinit var seekBar: SeekBar
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Initializing
-        val seekbar : SeekBar = findViewById(R.id.seek_bar)
-        val time_text : TextView = findViewById(R.id.time_left_text)
-        val title_text : TextView = findViewById(R.id.title)
-        val play_button : Button = findViewById(R.id.play_button)
-        val pause_button : Button = findViewById(R.id.pause_button)
-        val back_button : Button = findViewById(R.id.back_button)
-        val forward_button : Button = findViewById(R.id.forward_button)
+        val play_btn : Button = findViewById(R.id.play)
+        val stop_btn : Button = findViewById(R.id.pause)
+        val forward_btn: Button = findViewById(R.id.forward)
+        val back_btn : Button = findViewById(R.id.back)
+
+        val title_txt : TextView = findViewById(R.id.title)
+        time_txt  =findViewById(R.id.time_left_text)
+
+        seekBar  = findViewById(R.id.seek_bar)
+
+        // Media Player
+        mediaPlayer = MediaPlayer.create(
+            this,
+            R.raw.thoda_thoda
+        )
+
+        seekBar.isClickable = false
+
+        // Adding Functionalities for the buttons
+        play_btn.setOnClickListener(){
+            mediaPlayer.start()
+
+            finalTime = mediaPlayer.duration.toDouble()
+            startTime = mediaPlayer.currentPosition.toDouble()
+
+            if (oneTimeOnly == 0){
+                seekBar.max = finalTime.toInt()
+                oneTimeOnly = 1
+            }
+
+            time_txt.text = startTime.toString()
+            seekBar.setProgress(startTime.toInt())
+
+            handler.postDelayed(UpdateSongTime, 100)
+        }
 
 
+        // Setting the music title
+        title_txt.text = ""+ resources.getResourceEntryName(R.raw.thoda_thoda)
+
+        // Stop Button
+        stop_btn.setOnClickListener(){
+            mediaPlayer.pause()
+        }
+
+        // Forward Button
+        forward_btn.setOnClickListener(){
+            var temp = startTime
+            if ((temp + forwardTime) <= finalTime){
+                startTime = startTime + forwardTime
+                mediaPlayer.seekTo(startTime.toInt())
+            }else{
+                Toast.makeText(this,
+                    "Can't Jump forward", Toast.LENGTH_LONG).show()
+            }
+        }
+
+        back_btn.setOnClickListener(){
+            var temp = startTime.toInt()
+
+            if ((temp - backwardTime) >0){
+                startTime = startTime - backwardTime
+                mediaPlayer.seekTo(startTime.toInt())
+            }else{
+                Toast.makeText(this,
+                    "Can't Jump backward",
+                    Toast.LENGTH_LONG).show()
+            }
+        }
+
+    }
+
+    // Creating the Runnable
+    val UpdateSongTime: Runnable = object : Runnable {
+        override fun run() {
+            startTime = mediaPlayer.currentPosition.toDouble()
+            time_txt.text = "" +
+                    String.format(
+                        "%d min , %d sec",
+                        TimeUnit.MILLISECONDS.toMinutes(startTime.toLong()),
+                        TimeUnit.MILLISECONDS.toSeconds(
+                            startTime.toLong()
+                                    - TimeUnit.MINUTES.toSeconds(
+                                TimeUnit.MILLISECONDS.toMinutes(
+                                    startTime.toLong()
+                                )
+                            ))
+                    )
+            seekBar.progress = startTime.toInt()
+            handler.postDelayed(this, 100)
+
+        }
     }
 }
