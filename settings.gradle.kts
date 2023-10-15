64df59522d8568d6aa0e4575bbfb63cd60360f55
Shinigami <102893295+shadowlight671@users.noreply.github.com>
2023-10-15 07:48:15 +0600

    Demo Project

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Music_Player_Learning_Kotlin"
+include(":app")
